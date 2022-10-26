69d1e48aced89450448bd94be7b1f3af05f2bb71
Arion Dantas Jr <ariondantas027@gmail.com>
2022-10-26 00:06:56 -0300

    blocos de noticias alinhados

--- index.php
+++ index.php
@@ -112,15 +112,13 @@
             <ul class="list-unstyled">
               <li><a href="https://www.facebook.com/escolahunney" class="text-white"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
   <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
-</svg>  Curta nosso Facebook</a></li>
-              <li class="text-white"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+</svg> Curta nosso Facebook</a></li>
+              <li><a href="https://wa.me/5527996627479"  class="text-white"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
   <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
-</svg>  Whatsapp</li>
-              <li class="text-white">(27) 99999-9999</li>
-              <li class="text-white"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+</svg> Whatsapp</a></li>
+              <li> <a href="mailto:ariondantas027@gmail.com" class="text-white"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
   <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
-</svg>  Nosso e-mail</li>
-              <li class="text-white">emailescola@gmail.com</li>
+</svg> Nosso e-mail</a></li>
             </ul>
           </div>
         </div>
@@ -144,11 +142,11 @@
       <div class="container">
 
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col col-lg col-md col-sm" id="modalEleicoes">
-            <div class="card shadow-sm">
-              <img src="images/eleicoes/foto-cadidatos.jpg" class="img-fluid" alt="Hunney" style="width:100%; heigh: 255px;">
+          <div class="col col-lg col-md col-sm"  id="modalEleicoes">
+            <div class="card shadow-sm" style="width:100%; height:100%;">
+              <img src="images/eleicoes/foto-cadidatos.jpg" class="img-fluid" alt="Hunney" style="height: 100%;">
               <div class="card-body">
-              <p class="card-text"><a href="#eleicoes" id="eleicoes">Eleicões para presidencia.</a></p>
+              <p class="card-text"><a href="#eleicoes" id="eleicoes">Eleicões para presidência.</a></p>
                 <div class="d-flex justify-content-between align-items-center">
                   <div class="btn-group">
                   </div>
@@ -157,8 +155,8 @@
             </div>
           </div>
           <div class="col col-lg col-md col-sm" id="modalInterclasse">
-            <div class="card shadow-sm">
-              <img src="images/esporte/interclasse.jpg" alt="Hunney" style="width:100% height: 255px;">
+            <div class="card shadow-sm" style="width:100%; height:100%;">
+              <img src="images/esporte/interclasse.jpg" alt="Hunney" style="height:100%;">
               <div class="card-body">
                 <p class="card-text"><a href="#esporte-interclasse"> Esporte / Interclasse no Hunney.</a></p>
                 <div class="d-flex justify-content-between align-items-center">
@@ -170,7 +168,7 @@
           </div>
 
           <div class="col col-lg col-md col-sm" id="modalFeira">
-            <div class="card shadow-sm">
+            <div class="card shadow-sm" style="width:100%; height:100%;">
               <img src="images/feira/feira-de-negocios.jpg" alt="hunney">
               <div class="card-body">
                 <p class="card-text"><a href="#feira-de-negocios">1° Feira de negócios do Hunney.</a></p>
@@ -183,8 +181,8 @@
           </div>
 
           <div class="col col-lg col-md col-sm" id="modalPalestra"> 
-            <div class="card shadow-sm">
-                <img src="images/palestra/Sem título.png" alt="hunney">
+            <div class="card shadow-sm" style="width:100%; height:100%;">
+                <img src="images/palestra/Sem título.png" alt="hunney"style="height:100%;">
               <div class="card-body">
                 <p class="card-text"><a href="#palestra-prf">Palestra da PRF, no Hunney.</a></p>
                 <div class="d-flex justify-content-between align-items-center">
@@ -195,7 +193,7 @@
             </div>
           </div>
           <div class="col col-lg col-md col-sm" id="modalSemanaEstudante">
-            <div class="card shadow-sm">
+            <div class="card shadow-sm" style="width:100%; height:100%;">
               <img src="images/semana-estudante/semana-do-estudante.png" alt="hunney">
               <div class="card-body">
                 <p class="card-text"><a href="#semana-do-estudante">Semana do Estudante.</a></p>
@@ -207,8 +205,8 @@
             </div>
           </div>
           <div class="col col-lg col-md col-sm">
-            <div class="card shadow-sm">
-            <img src="images/logo-poli/hunney-everest-piovesan.png" alt="hunney">
+            <div class="card shadow-sm"style="width:100%; height:100%;">
+            <img src="images/logo-poli/hunney-everest-piovesan.png" alt="hunney" style="height:100%;">
               <div class="card-body">
                 <p class="card-text"><a href="#noticias-em-breve">Notícias em breve.</a></p>
                 <div class="d-flex justify-content-between align-items-center">
